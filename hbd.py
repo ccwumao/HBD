1e38aaaa1de403a4836e6a5defdf213223b863e4
jaredporter <jared.r.porter@gmail.com>
2016-08-24 22:52:42 +0100

    finished the installation check, updated run_it()

--- hbd.py
+++ hbd.py
@@ -13,17 +13,20 @@ class Birthday(object):
         self.name = ''
         
     def espeak_checker(self):
-        cmd = "where" if self.os == "Windows" else "which"#
+        # Check to see if espeak is installed
+        cmd = "where" if self.os == "Windows" else "which"
+        # 1 if not installed 0 if installed
         rseponse = call([cmd, 'espeak'])
+        # Install instructions for Linux and return False
         if rseponse == 1 and self.os == "Linux":
             print("Looks like you don't have espeak installed. You can \
             install it with sudo apt-get install espeak")
             return False
+        # Install instructions for Windows and return False
         elif rseponse == 1 and self.os == "Windows":
-            print("Looks like you don't have espeak installed. You can download it from \
-            http://sourceforge.net/projects/espeak/files/espeak/espeak-1.48/setup_espeakedit-1.48.03.exe \
-            but be sure to add it to your path when you're finished intalling it!")
+            print("Looks like you don't have espeak installed. You can download it from http://sourceforge.net/projects/espeak/files/espeak/espeak-1.48/setup_espeakedit-1.48.03.exe but be sure to add it to your path when you're finished intalling it!")
             return False
+        # Return True
         elif response == 0:
             return True
     
@@ -58,14 +61,15 @@ class Birthday(object):
         system('espeak {}'.format(self.hbd_string()))
         
     def run_it(self):
-        self.input_string()
-        time.sleep(2)
-        if self.os == 'Windows' or self.os == 'Linux':
-            self.hbd_linux_windows()
-        elif self.os == 'Darwin':
-            self.hbd_mac()
-        else:
-            print("Hey, looks like you're not on Windows, Linux, or \
+        if self.espeak_checker():
+            self.input_string()
+            time.sleep(2)
+            if self.os == 'Windows' or self.os == 'Linux':
+                self.hbd_linux_windows()
+            elif self.os == 'Darwin':
+                self.hbd_mac()
+            else:
+                print("Hey, looks like you're not on Windows, Linux, or \
 a Mac. So I guess I'll just say Happy Birthday, {}!".format(self.name))
         
     
@@ -74,7 +78,4 @@ if __name__ == '__main__':
     try:
         BDay.run_it()
     except:
-        print("I think you're missing espeak. If you're on Linux, try \
-sudo apt-get install espeak. For Windows it's at this address: \
-http://sourceforge.net/projects/espeak/files/espeak/espeak-1.48/setup_espeakedit-1.48.03.exe")
-        print("Anyway, Happy Birthday, {}!".format(BDay.name))
+        print("That's weird, something isn't working. I thought I accounted for everything. Oh well, Happy Birthday, {}!".format(BDay.name))
